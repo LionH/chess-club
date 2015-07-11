11f2fc9b2761dbd534f9ec3e158e2687f2ef2a39
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2015-07-11 16:47:00 +0200

    Bootstrap job to create a basic set of players.

---
+++ src/main/java/org/chesscorp/club/jobs/Bootstrap.java
@@ -0,0 +1,24 @@
+package org.chesscorp.club.jobs;
+
+
+import org.chesscorp.club.model.Player;
+import org.chesscorp.club.persistence.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class Bootstrap {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @PostConstruct
+    public void populate() {
+        playerRepository.save(new Player("alcibiade", "Alcibiade"));
+        playerRepository.save(new Player("john", "John"));
+        playerRepository.save(new Player("bob", "Bob"));
+        playerRepository.save(new Player("steve", "Steve"));
+    }
+}
