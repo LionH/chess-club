f5de9710650f991b987f261f3d15918e5b519afb
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2015-07-11 21:56:45 +0200

    First draft of Facebook integration.

---
+++ src/main/java/org/chesscorp/club/config/SecurityConfig.java
@@ -0,0 +1,78 @@
+package org.chesscorp.club.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.social.config.annotation.EnableSocial;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebMvcSecurity
+@EnableSocial
+@Profile("facebook")
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Inject
+    private DataSource dataSource;
+
+    @Autowired
+    public void registerAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, true from Account where username = ?")
+                .authoritiesByUsernameQuery("select username, 'ROLE_USER' from Account where username = ?")
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/**/*.css", "/**/*.png", "/**/*.gif", "/**/*.jpg");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin()
+                .loginPage("/signin")
+                .loginProcessingUrl("/signin/authenticate")
+                .failureUrl("/signin?param.error=bad_credentials")
+                .and()
+                .logout()
+                .logoutUrl("/signout")
+                .deleteCookies("JSESSIONID")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and()
+                .rememberMe();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public TextEncryptor textEncryptor() {
+        return Encryptors.noOpText();
+    }
+}
