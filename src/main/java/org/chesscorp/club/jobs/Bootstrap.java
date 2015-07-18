bbcf85d62aa1d20cd8fa69a6ea650b25b910ce40
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2015-07-18 17:43:38 +0200

    Isolate bootstrap process in a dedicated Spring profile.

--- src/main/java/org/chesscorp/club/jobs/Bootstrap.java
+++ src/main/java/org/chesscorp/club/jobs/Bootstrap.java
@@ -10,11 +10,13 @@ import org.chesscorp.club.persistence.PlayerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component
+@Profile("bootstrap")
 public class Bootstrap {
 
     private Logger logger = LoggerFactory.getLogger(Bootstrap.class);
