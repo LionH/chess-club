46b67a26a886336faea1e42a80aaefc0b1b4ac3e
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2015-09-23 19:47:31 +0200

    Add a filter to provide runtime CORS headers. Integration of Mockito to help create unit tests for the filter.

---
+++ src/main/java/org/chesscorp/club/filters/CorsFilter.java
@@ -0,0 +1,32 @@
+package org.chesscorp.club.filters;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Provides CORS informations so that the service can be used from different origins.
+ */
+@Component
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
---
+++ src/test/java/org/chesscorp/club/filters/CorsFilterTest.java
@@ -0,0 +1,41 @@
+package org.chesscorp.club.filters;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by yk on 23/09/15.
+ */
+
+public class CorsFilterTest {
+
+    @Test
+    public void testInitAndDestroy() throws IOException, ServletException {
+        CorsFilter filter = new CorsFilter();
+
+        FilterConfig filterConfig = Mockito.mock(FilterConfig.class);
+        filter.init(filterConfig);
+        filter.destroy();
+    }
+
+    @Test
+    public void testFilter() throws IOException, ServletException {
+        CorsFilter filter = new CorsFilter();
+
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+        FilterChain filterChain = Mockito.mock(FilterChain.class);
+
+        filter.doFilter(request, response, filterChain);
+
+        Mockito.verify(filterChain, Mockito.times(1)).doFilter(request, response);
+        Mockito.verify(response, Mockito.times(4)).setHeader(Mockito.any(String.class), Mockito.any(String.class));
+    }
+}
