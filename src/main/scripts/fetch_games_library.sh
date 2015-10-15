977bafdd54d28a6ca0dea1257b78e9e455d551d1
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2015-10-15 12:09:59 +0200

    Add a script to fetch PGN games library from internet sites.

---
+++ src/main/scripts/fetch_games_library.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p import
+cd import
+
+PLAYERS_BASE=$(curl http://www.pgnmentor.com/files.html | grep "players/.*zip" | sed -e 's/\.zip.*//' -e 's/.*href="players\///')
+
+for base in $PLAYERS_BASE
+do
+    echo "Fetching $base"
+    curl -o ${base}.zip http://www.pgnmentor.com/players/${base}.zip
+    unzip ${base}.zip
+    rm -f ${base}.zip
+done
