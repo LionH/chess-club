aa46f3f93157ccb7bdd7d78eab7e3fbe255ab86b
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2015-10-16 10:18:20 +0200

    Re-activate the bootstrap test but with a cleanup procedure this time.

--- src/test/java/org/chesscorp/club/jobs/BootstrapTest.java
+++ src/test/java/org/chesscorp/club/jobs/BootstrapTest.java
@@ -3,28 +3,35 @@ package org.chesscorp.club.jobs;
 import org.assertj.core.api.Assertions;
 import org.chesscorp.club.Application;
 import org.chesscorp.club.model.Robot;
+import org.chesscorp.club.persistence.AccountRepository;
 import org.chesscorp.club.persistence.ChessGameRepository;
 import org.chesscorp.club.persistence.PlayerRepository;
 import org.chesscorp.club.persistence.RobotRepository;
-import org.junit.Ignore;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Test the bootstrap process that initializes the storage contents.
+ * <br/>
+ * Note that this test has to manually cleanup all its data because the bootstrap
+ * will create data out of the test's transaction scope.
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-@TransactionConfiguration(defaultRollback = true)
-@Ignore
+@TransactionConfiguration(defaultRollback = false)
+@ActiveProfiles(profiles = "bootstrap")
 public class BootstrapTest {
+
+    @Autowired
+    private AccountRepository accountRepository;
 
     @Autowired
     private PlayerRepository playerRepository;
@@ -35,15 +42,19 @@ public class BootstrapTest {
     @Autowired
     private ChessGameRepository chessGameRepository;
 
-    @Autowired
-    private ObjectFactory<Bootstrap> bootstrapObjectFactory;
+    @After
+    @Transactional
+    public void cleanup() {
+        accountRepository.deleteAll();
+        chessGameRepository.deleteAll();
+        playerRepository.deleteAll();
+    }
 
     @Test
     @Transactional
     public void testSampleData() {
-        Bootstrap bootstrap = bootstrapObjectFactory.getObject();
-        bootstrap.populate();
         Assertions.assertThat(playerRepository.findAll()).hasSize(5).hasAtLeastOneElementOfType(Robot.class);
+        Assertions.assertThat(accountRepository.findAll()).hasSize(2);
         Assertions.assertThat(robotRepository.findAll()).hasSize(1);
         Assertions.assertThat(chessGameRepository.findAll()).hasSize(2);
     }
