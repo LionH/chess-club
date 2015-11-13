4b3b91442e15c10be38a856ac5e5e68d02955dff
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2015-11-13 17:21:41 +0100

    Update the performance monitor log format align duration logs.

--- src/main/java/org/chesscorp/club/monitoring/PerformanceMonitorImpl.java
+++ src/main/java/org/chesscorp/club/monitoring/PerformanceMonitorImpl.java
@@ -27,7 +27,7 @@ public class PerformanceMonitorImpl implements PerformanceMonitor {
     @Override
     public void register(String component, String operation, long items, String itemType, long duration) {
 
-        String message = String.format("%20s - %-16s : %5d %s(s) in %.3fs (%6.1f %s(s)/s)",
+        String message = String.format("%20s - %-16s : %5d %s(s) in %8.3fs (%6.1f %s(s)/s)",
                 component, operation, items, itemType, duration / 1000., 1000. * items / duration, itemType);
 
         if (items == 0) {
