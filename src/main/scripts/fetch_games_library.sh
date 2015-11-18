4acd08ce11fa2b53804c61f4b839828acfdd8f71
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2015-11-18 21:49:40 +0100

    Ignore the Paehtz pgn archive as it contains a corrupted game.

--- src/main/scripts/fetch_games_library.sh
+++ src/main/scripts/fetch_games_library.sh
@@ -3,7 +3,7 @@
 mkdir -p import
 cd import
 
-PLAYERS_BASE=$(curl http://www.pgnmentor.com/files.html | grep "players/.*zip" | sed -e 's/\.zip.*//' -e 's/.*href="players\///')
+PLAYERS_BASE=$(curl http://www.pgnmentor.com/files.html | grep "players/.*zip" | sed -e 's/\.zip.*//' -e 's/.*href="players\///' | grep -v Paehtz)
 
 for base in $PLAYERS_BASE
 do
