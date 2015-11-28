76331d0a65ef0c040a444d7a4d4b1e44a1bcd423
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2015-11-28 19:02:05 +0100

    Added a global controller exception handler.

---
+++ src/main/java/org/chesscorp/club/controllers/GlobalControllerExceptionHandler.java
@@ -0,0 +1,19 @@
+package org.chesscorp.club.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Handle all controller exceptions.
+ */
+@ControllerAdvice
+class GlobalControllerExceptionHandler {
+    private Logger logger = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public void handleException(Exception e) {
+        logger.info(e.getLocalizedMessage());
+    }
+}
