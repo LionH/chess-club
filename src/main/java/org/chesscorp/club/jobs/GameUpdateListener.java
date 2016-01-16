a26ca612a37b680132443d13cbfdf434246236a2
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2016-01-16 18:54:51 +0100

    Replace the position update polling job with a game update listener.

--- src/main/java/org/chesscorp/club/jobs/GameUpdateListener.java
+++ src/main/java/org/chesscorp/club/jobs/GameUpdateListener.java
@@ -1,9 +1,14 @@
 package org.chesscorp.club.jobs;
 
+import org.chesscorp.club.monitoring.PerformanceMonitor;
+import org.chesscorp.club.service.ChessPositionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Game update post-processing.
@@ -12,8 +17,18 @@ import org.springframework.stereotype.Component;
 public class GameUpdateListener {
     private Logger logger = LoggerFactory.getLogger(GameUpdateListener.class);
 
+    @Autowired
+    private ChessPositionService chessPositionService;
+
+    @Autowired
+    private PerformanceMonitor performanceMonitor;
+
     @JmsListener(destination = "chess-game-update")
+    @Transactional(propagation = Propagation.NEVER)
     public void gameUpdated(String message) {
         logger.debug("Game {} updated", message);
+        performanceMonitor.mark();
+        long moves = chessPositionService.updateMovePositions();
+        performanceMonitor.register("IndexUpdateJob", "update-positions", moves, "move");
     }
 }
