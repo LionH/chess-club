416c68e9f21350dd31700a326bae53af9a3b57ff
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2016-06-24 11:00:41 +0200

    Update chess library dependency version to 1.6 to get access to analytical engines.

--- src/main/java/org/chesscorp/club/config/ChessConfig.java
+++ src/main/java/org/chesscorp/club/config/ChessConfig.java
@@ -3,7 +3,6 @@ package org.chesscorp.club.config;
 import org.alcibiade.chess.engine.CraftyEngineImpl;
 import org.alcibiade.chess.engine.GnuChessEngineImpl;
 import org.alcibiade.chess.engine.PhalanxEngineImpl;
-import org.alcibiade.chess.engine.process.ExternalProcessFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,14 +12,8 @@ import org.springframework.context.annotation.Profile;
  * Chess related components initialization.
  */
 @Configuration
-@ComponentScan({"org.alcibiade.chess.persistence", "org.alcibiade.chess.rules"})
+@ComponentScan({"org.alcibiade.chess.persistence", "org.alcibiade.chess.rules", "org.alcibiade.chess.engine.process"})
 public class ChessConfig {
-
-
-    @Bean
-    public ExternalProcessFactory getExternalProcessFactory() {
-        return new ExternalProcessFactory();
-    }
 
     @Bean
     public GnuChessEngineImpl getGnuChessEngine() {
