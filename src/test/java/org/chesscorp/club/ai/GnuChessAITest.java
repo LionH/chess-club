4c9fd7584b6cdac247fcf000aa434c24cb507422
LionH <liontes@gmail.com>
2016-09-11 12:49:18 +0200

    misspelled profile name

--- src/test/java/org/chesscorp/club/ai/GnuChessAITest.java
+++ src/test/java/org/chesscorp/club/ai/GnuChessAITest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-@ActiveProfiles("gnuchess")
+@ActiveProfiles("ai-gnuchess")
 @Rollback
 public class GnuChessAITest {
 
--- src/test/java/org/chesscorp/club/ai/PhalanxAITest.java
+++ src/test/java/org/chesscorp/club/ai/PhalanxAITest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-@ActiveProfiles("phalanx")
+@ActiveProfiles("ai-phalanx")
 @Rollback
 public class PhalanxAITest {
 
